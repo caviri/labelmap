927cee94e229c7bf7cace6107f41ffa0b0141404
Carlos Vivar <carlos.vivarrios@unil.ch>
2022-11-12 17:07:44 +0100

    app ready for testing with JSON download

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,9 @@ from streamlit_folium import st_folium
 import numpy as np
 import pandas as pd
 import datetime
+import json
+
+import io
 
 st.set_page_config(page_title="Labelmap test", page_icon="🌍", layout="wide", initial_sidebar_state="expanded")
 st.markdown("# Labelmap test")
@@ -18,6 +21,10 @@ df = pd.DataFrame(
 
 st.dataframe(df)
 
+# Title
+
+title = st.text_input('Title', 'Life of Brian')
+st.write('The current movie title is', title)
 
 # Dateselect
 d = st.date_input(
@@ -35,15 +42,11 @@ options = st.multiselect(
 st.write('You selected:', options)
 
 # Text area
-txt = st.text_area('Text to analyze', '''
-    It was the best of times, it was the worst of times, it was
-    the age of wisdom, it was the age of foolishness, it was
-    the epoch of belief, it was the epoch of incredulity, it
-    was the season of Light, it was the season of Darkness, it
-    was the spring of hope, it was the winter of despair, (...)
-    ''')
+txt = st.text_area('Text to analyze', '''It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, (...) ''', height=500)
 
+st.write('Your birthday is:', txt)
 # Map
+
 m = folium.Map(location=[39.949610, -75.150282], zoom_start=5)
 Draw(export=True).add_to(m)
 
@@ -54,14 +57,26 @@ with c1:
 with c2:
     st.json(output)
 
+import base64
 
 # Upload multiples files
 uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
 for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
+    #bytes_data = uploaded_file.read()
+    #stringio = StringIO(uploaded_file.getvalue().decode("base64")).read()
+    stringio = base64.b64encode(uploaded_file.getvalue()).decode()
     st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
 
+from PIL import Image
+
+if len(uploaded_files) == 0:
+    stringio = 'None'
+else:
+    # img = Image.open(io.BytesIO(base64.b64decode(stringio)))
+    # st.image(img)
+    st.video(io.BytesIO(base64.b64decode(stringio)))
+
+    
 ## Single file uploader
 # uploaded_file = st.file_uploader("Choose a file")
 # if uploaded_file is not None:
@@ -80,3 +95,22 @@ for uploaded_file in uploaded_files:
 #     # Can be used wherever a "file-like" object is accepted:
 #     dataframe = pd.read_csv(uploaded_file)
 #     st.write(dataframe)
+
+## Download
+@st.cache
+def createJSON():
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    #mapinfo = json.loads(output)
+    dicts = {'title': title, 'options': options, 'map': output, 'file': stringio}
+    js = json.dumps(dicts)
+    return js
+
+
+
+js = createJSON()
+st.download_button(
+    label="Download data as JSON",
+    data=js,
+    file_name='data.json',
+    mime='text/json',
+)
